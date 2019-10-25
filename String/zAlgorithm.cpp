10a0b821aa564e9989142e4d153fb5a7c2a4fa31
irfansofyana <irfansofyana0305@gmail.com>
2019-10-25 20:21:49 +0700

    fix Z algorithm

--- String/zAlgorithm.cpp
+++ String/zAlgorithm.cpp
@@ -1,19 +1,33 @@
-string s; cin >> s;
-int L = 0, R = 0;
-int n = s.size();
-for (int i = 1; i < n; ++i) {
-    if (i > R) {
-        L = R = i;
-        while (R < n && s[R] == s[R-L]) ++R;
-        Z[i] = R-L; --R;
+void search(string text, string pattern) 
+{ 
+    string concat = pattern + "$" + text; 
+    int l = concat.length(); 
+    int Z[l]; 
+    getZarr(concat, Z); 
+    for (int i = 0; i < l; ++i) { 
+        if (Z[i] == pattern.length()) 
+            cout << "Pattern found at index "
+                << i - pattern.length() -1 << endl; 
     }
-    else {
-        int k = i-L;
-        if (Z[k] < R-i+1) Z[i] = Z[k];
-        else {
-            L = i;
+} 
+
+void getZarr(string s, Z[]){
+    int L = 0, R = 0;
+    int n = s.size();
+    for (int i = 1; i < n; ++i) {
+        if (i > R) {
+            L = R = i;
             while (R < n && s[R] == s[R-L]) ++R;
             Z[i] = R-L; --R;
+        }
+        else {
+            int k = i-L;
+            if (Z[k] < R-i+1) Z[i] = Z[k];
+            else {
+                L = i;
+                while (R < n && s[R] == s[R-L]) ++R;
+                Z[i] = R-L; --R;
+            }
         }
     }
 }
